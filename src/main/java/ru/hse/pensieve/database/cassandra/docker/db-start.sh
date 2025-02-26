87df89f6c75a29e746300a92292508980c682635
kolesovasonya <soffakolesova@gmail.com>
2025-02-26 18:28:39 +0300

    database: fix cassandra architecture and add models and repos

--- src/main/java/ru/hse/pensieve/database/cassandra/docker/db-start.sh
+++ src/main/java/ru/hse/pensieve/database/cassandra/docker/db-start.sh
@@ -1,3 +1,6 @@
 #!bin/bash
 
 docker-compose up -d
+docker exec -it cassandra bash -c "sed -i 's/materialized_views_enabled: false/materialized_views_enabled: true/' /etc/cassandra/cassandra.yaml"
+docker exec -it cassandra bash -c "sed -i 's/materialized_views_enabled: false/materialized_views_enabled: true/' /etc/cassandra/cassandra_latest.yaml"
+docker restart cassandra
