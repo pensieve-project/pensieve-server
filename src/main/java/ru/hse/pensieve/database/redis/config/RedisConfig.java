686568c03a7760d77a5cf3bfcf86cedb79e6d3f6
Alina Chernokulskaia <aachernokulskaia@edu.hse.ru>
2025-06-03 20:51:25 +0300

    feat: popular posts feed

--- src/main/java/ru/hse/pensieve/database/redis/config/RedisConfig.java
+++ src/main/java/ru/hse/pensieve/database/redis/config/RedisConfig.java
@@ -1,10 +1,12 @@
 package ru.hse.pensieve.database.redis.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import ru.hse.pensieve.database.cassandra.models.Post;
 
@@ -16,7 +18,11 @@ public class RedisConfig {
         RedisTemplate<String, Post> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        Jackson2JsonRedisSerializer<Post> serializer = new Jackson2JsonRedisSerializer<>(objectMapper, Post.class);
+        template.setValueSerializer(serializer);
+        template.afterPropertiesSet();
         return template;
     }
 }
