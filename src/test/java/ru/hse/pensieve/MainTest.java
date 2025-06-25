3d636ef456180f45cf5fe45646d6d98553128604
Golovachev Sergey <golovacev905@gmail.com>
2025-06-25 19:39:25 +0300

    style: style fixes

--- src/test/java/ru/hse/pensieve/MainTest.java
+++ src/test/java/ru/hse/pensieve/MainTest.java
@@ -7,8 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MainTest {
 
-//    @Test
-//    void contextLoads() {
-//    }
-
+    @Test
+    void contextLoads() {}
 }
